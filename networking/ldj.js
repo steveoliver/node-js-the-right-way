9b48812181066a5f6f6ef22854fcd2543a4c3db0
Steve Oliver <steve@circatree.com>
2014-07-12 11:51:59 -0700

    Page 35.

---
+++ networking/ldj.js
@@ -0,0 +1,9 @@
+const
+  events = require('events'),
+  util = require('util'),
+  // client constructor
+  LDJClient = function(stream) {
+    events.EventEmitter.call(this);
+  };
+util.inherits(LDJClient, events.EventEmitter);
+
