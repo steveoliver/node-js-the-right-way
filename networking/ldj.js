2c1317b4d26adb4647d7329db231b1a0e1517e6a
Steve Oliver <steve@circatree.com>
2014-07-12 12:21:56 -0700

    Page 36 fix.

--- networking/ldj.js
+++ networking/ldj.js
@@ -1,3 +1,4 @@
+"use strict";
 const
   events = require('events'),
   util = require('util'),
@@ -11,16 +12,17 @@ const
       buffer += data;
       let boundary = buffer.indexOf('\n');
       while (boundary !== -1) {
-       let input = buffer.substr(0, boundary);
-       buffer = buffer.substr(boundary + 1);
-       self.emit('message', JSON.parse(input));
-       boundary = buffer = indexOf('\n');
+        let input = buffer.substr(0, boundary);
+        buffer = buffer.substr(boundary + 1);
+        self.emit('message', JSON.parse(input));
+        boundary = buffer.indexOf('\n');
+      }
     });
   };
 util.inherits(LDJClient, events.EventEmitter);
 
 // expose module methods
-exports.LDJClient = LDJclient;
+exports.LDJClient = LDJClient;
 exports.connect = function(stream) {
   return new LDJClient(stream);
 };
